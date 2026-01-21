0380b8f0e053621c4b3692231ef30142592b1641
OtavioAugustoMonteiroDiscini <otavio2005discini@gmail.com>
2026-01-21 18:53:09 -0300

    feat: enable CORS middleware in backend application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import router from './routes/convert.routes';
+import cors from "cors";
 
 export const app = express();
 
+app.use(cors()); 
 app.use(express.json());
 app.use(router);
